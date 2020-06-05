a56205eeb861fcfc9fbd20199faa46b340a21990
Pedro Vivas <pmvp82@gmail.com>
2020-06-05 17:30:36 -0400

    Updates

---
+++ precache-manifest.3a259dffded08bdf7fc37b2faf4798ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3564879a16b77bcedc599ed4cc87f66",
+    "url": "/phone-store/index.html"
+  },
+  {
+    "revision": "c3ab060c3faab61da264",
+    "url": "/phone-store/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "862e996cf039cd0dde18",
+    "url": "/phone-store/static/css/main.1e93f5d5.chunk.css"
+  },
+  {
+    "revision": "c3ab060c3faab61da264",
+    "url": "/phone-store/static/js/2.a586e18c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/phone-store/static/js/2.a586e18c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "862e996cf039cd0dde18",
+    "url": "/phone-store/static/js/main.16c9a13c.chunk.js"
+  },
+  {
+    "revision": "b3d9921209112036f31f",
+    "url": "/phone-store/static/js/runtime-main.87aa7d60.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/phone-store/static/media/logo.9f7e506d.svg"
+  }
+]);
