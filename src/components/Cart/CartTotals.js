e12ba67cdcaaee0e86ffb86692511d23b2a0c1c0
Pedro Vivas <pmvp82@gmail.com>
2020-07-10 00:34:26 -0400

    update 09-07

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -6,37 +6,42 @@ import styled from 'styled-components'
 export default function CartTotals({value, history}) {
     const { cartSubTotal, cartTax, cartTotal, clearCart } = value
     return (
-        <React.Fragment>
-            <div className="container">
-                <div className="row">
-                    <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
-                        <Link to="/phone-store">
-                            <ClearButton
-                                className="btn btn-outline-danger text-uppercase mb-3 px-5"
-                                type="button"
-                                onClick={() => clearCart()}
-                            >
-                                clear cart
-                            </ClearButton>
-                        </Link>
-                        <h5>
-                            <span className="text-title">subtotal: </span>
-                            <strong>${cartSubTotal}</strong>
-                        </h5>
-                        <h5>
-                            <span className="text-title">tax: </span>
-                            <strong>${cartTax}</strong>
-                        </h5>
-                        <h5>
-                            <span className="text-title">total: </span>
-                            <strong>${cartTotal}</strong>
-                        </h5>
-                        <PayPalButton total={cartTotal} clearCart={clearCart} history={history} />
-                    </div>
-                </div>
+      <React.Fragment>
+        <div className="container">
+          <div className="row">
+            {/*<div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">*/}
+            <div className="col-10 mt-2 ml-auto col-sm-8 text-capitalize text-right">
+              <Link to="/phone-store">
+                <ClearButton
+                  className="btn btn-outline-danger text-uppercase mb-3 px-5"
+                  type="button"
+                  onClick={() => clearCart()}
+                >
+                  clear cart
+                </ClearButton>
+              </Link>
+              <h5>
+                <span className="text-title">subtotal: </span>
+                <strong>${cartSubTotal}</strong>
+              </h5>
+              <h5>
+                <span className="text-title">tax: </span>
+                <strong>${cartTax}</strong>
+              </h5>
+              <h5>
+                <span className="text-title">total: </span>
+                <strong>${cartTotal}</strong>
+              </h5>
+              <PayPalButton
+                total={cartTotal}
+                clearCart={clearCart}
+                history={history}
+              />
             </div>
-        </React.Fragment>
-    )
+          </div>
+        </div>
+      </React.Fragment>
+    );
 }
 
 const ClearButton = styled.button`
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,26 +6,30 @@ import { ProductConsumer } from '../context'
 export default class ProductList extends Component {
     render() {
         return (
-            <React.Fragment>
-                <div>
-                    <img src="/phone-store/img/banner.jpg" alt="Store Banner" className="img-fluid" /> 
+          <React.Fragment>
+            <div>
+              <img
+                src="/phone-store/img/banner.jpg"
+                alt="Store Banner"
+                className="img-fluid"
+              />
+            </div>
+            <div className="py-5">
+              <div className="container">
+                <Title name="our" title="products" />
+                <div className="row">
+                  <ProductConsumer>
+                    {(value) => {
+                      return value.products.map((product) => {
+                        return <Product key={product.id} product={product} />;
+                      });
+                    }}
+                  </ProductConsumer>
                 </div>
-                <div className="py-5">
-                    <div className="container">
-                        <Title name="our" title="products" />
-                        <div className="row">
-                           <ProductConsumer>
-                               {(value) => {
-                                   return value.products.map(product => {
-                                       return <Product key={product.id} product={product} />
-                                   })
-                               }}
-                           </ProductConsumer>
-                        </div>
-                    </div>
-                </div>
-            </React.Fragment>
-        )
+              </div>
+            </div>
+          </React.Fragment>
+        );
     }
 }
 
